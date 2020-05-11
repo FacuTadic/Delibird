7b76306b3620abee5508cc8e1139e58c47a66d9b
Milton Martorello <miltonmartorello@gmail.com>
2020-05-11 00:16:23 -0300

    Team inicializado

--- Team/src/team.c
+++ Team/src/team.c
@@ -1,63 +1,68 @@
 /*
- ============================================================================
- Name        : team.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * main.c
+ *
+ *  Created on: 28 feb. 2019
+ *      Author: utnso
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "tp0.h"
 #include <commons/string.h>
-#include <commons/collections/queue.h>
-#include <commons/collections/list.h>
+#include <stdio.h>
+#include "utils.h"
 
+int crearEntrenadores(char** objetivos,t_log* logger);
 t_config* cargarConfigEntrenadores(){
 	t_config* entrenadores;
 	entrenadores = config_create("entrenadores.config");
 	return entrenadores;
+
 }
 
 int main(void){
 	int conexion;
-	int cantEntrenadores;
+
 	t_log* logger;
 	t_config* config;
-	t_config* entrenadoresCFG;
-	t_list* entrenadores;
-	t_list* objetivosTeam;
+	t_config* entrenadores;
 
 	logger = iniciar_logger();
+	log_info(logger, "logger iniciado.");
+
 	config = leer_config();
+	log_info(logger, "config cargada.");
 
 	char* ip 		= config_get_string_value(config, "IP");
 	char* puerto 	= config_get_string_value(config, "PUERTO");
+	log_info(logger, "La IP es: %s", ip);
+	log_info(logger, "El Puerto es: %s", puerto);
 
 	conexion = crear_conexion(ip, puerto);
 
-	entrenadores = list_create();
-	entrenadoresCFG = cargarConfigEntrenadores(); // Cargamos la config
-	cantEntrenadores = config_keys_amount(entrenadoresCFG); // obtenemos la cardinalidad
-	char** objetivos = config_get_array_value(entrenadoresCFG, "OBJETIVOS_ENTRENADORES"); // obtenemos los objetivos
-	t_entrenador* trainer;
-	list_add(entrenadores, trainer);
-	log_info(logger, "La cantidad de entrenadores a cargar es de: %d", cantEntrenadores);
-	log_info(logger, "lista creada");
+	entrenadores = cargarConfigEntrenadores();
+	int cantEntrenadores = config_keys_amount(entrenadores);
+	char** objetivos = config_get_array_value(entrenadores, "OBJETIVOS_ENTRENADORES");
+	//int i = crearEntrenadores(objetivos, logger);
+
+
+	//log_info(logger, "encontre %d", cantEntrenadores);
+	//log_info(logger, "hay %d objetivos", i);
 
 
 
-//		funciones del TP
+
+
 //		conexion = crear_conexion(ip, puerto);
 //		enviar_mensaje("Pikachu", conexion,logger); // enviar_get ("pokemon", conexion);
 //		char* mensajeRecibido = recibir_mensaje(conexion, logger); // el ID
 //		log_info(logger,"el mensaje recibido es: %s" ,mensajeRecibido);
 //		free (mensajeRecibido);
 //		liberar_conexion(conexion);
+
+
 	terminar_programa(conexion, logger, config);
 }
+
+
 
 void terminar_programa(int conexion, t_log* logger, t_config* config){
 	log_destroy(logger);
@@ -66,10 +71,12 @@ void terminar_programa(int conexion, t_log* logger, t_config* config){
 
 }
 
+
 t_log* iniciar_logger(void){
 	t_log* logger = log_create("tp0.log", "Game-watch-client", 1, LOG_LEVEL_INFO);
 	return logger;
 }
+
 
 t_config* leer_config(){
 		t_config* config;
