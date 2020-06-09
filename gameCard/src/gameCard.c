45baed279dad5f20d26b39a2323f18ffa2cda9ca
FacuTadic <facutadich@gmail.com>
2020-06-08 23:54:33 -0300

    creacion de directorios, de archivo metadata FIle y estructura de mensajes

--- gameCard/src/gameCard.c
+++ gameCard/src/gameCard.c
@@ -10,8 +10,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "utilsGameCard.h"
+
+t_log* iniciar_loggerDEV(){
+	//t_log* logger = log_create("gameBoy.log", "Game-watch-client", 1, LOG_LEVEL_INFO);
+	return log_create("gameCardDEV.log", "Game-watch-client", 1, LOG_LEVEL_INFO);
+}
+
+
+t_log* iniciar_loggerGameCard(){
+	//t_log* logger = log_create("gameBoy.log", "Game-watch-client", 1, LOG_LEVEL_INFO);
+	return log_create("gameCard.log", "Game-watch-client", 1, LOG_LEVEL_INFO);
+}
+
+t_config* leer_config(){
+		t_config* config;
+		config = config_create("gameCard.config");
+		return config;
+}
+
+
+void terminar_programa(int conexion);
+
+
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+	int conexion;
+	ipBroker = string_new();
+	puertoBroker = string_new();
+
+	loggerGameCard = iniciar_loggerGameCard();
+	log_info(loggerGameCard, "logger iniciado.");
+
+	loggerDev = iniciar_loggerDEV();
+	log_info(loggerDev, "logger iniciado.");
+
+	config = leer_config();
+	log_info(loggerDev, "Config cargada.");
+
+	ipBroker = config_get_string_value(config, "IP_BROKER");
+	log_info(loggerDev, "La IP del Broker: %s", ipBroker);
+
+	puertoBroker = config_get_string_value(config, "PUERTO_BROKER");
+	log_info(loggerDev, "El Puerto es: %s", puertoBroker);
+
+	tiempoReintentoConexion = atoi(config_get_string_value(config, "TIEMPO_DE_REINTENTO_CONEXION"));
+	log_info(loggerDev, "El tiempo de reintento de conexion es: %i", tiempoReintentoConexion);
+
+	tiempoReintentoOperacion = atoi(config_get_string_value(config, "TIEMPO_DE_REINTENTO_OPERACION"));
+	log_info(loggerDev, "El tiempo de reintento de operacion es: %i", tiempoReintentoOperacion);
+
+
+	tiempoRetardoOperacion = atoi(config_get_string_value(config, "TIEMPO_RETARDO_OPERACION"));
+	log_info(loggerDev, "El tiempo de retardo de conexion es: %i", tiempoRetardoOperacion);
+
+	puntoMontaje = config_get_string_value(config, "PUNTO_MONTAJE_TALLGRASS");
+	log_info(loggerDev, "El punto de montaje es: %s", puntoMontaje);
+	//crearPuntoMontaje(puntoMontaje);
+	//string_append(&puntoMontaje, "/");
+
+	levantarTallGrass(puntoMontaje);
+}
+
+
+void terminar_programa(int conexion){
+	log_destroy(loggerDev);
+	log_destroy(loggerGameCard);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
--- gameCard/src/utilsGameCard.h
+++ gameCard/src/utilsGameCard.h
@@ -4,10 +4,171 @@
  *  Created on: 26 may. 2020
  *      Author: utnso
  */
-
 #ifndef UTILSGAMECARD_H_
 #define UTILSGAMECARD_H_
 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<commons/log.h>
+#include<commons/config.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+
+char* ipBroker;
+char* puertoBroker;
+char* puntoMontaje;
+uint32_t tiempoReintentoConexion;
+uint32_t tiempoReintentoOperacion;
+uint32_t tiempoRetardoOperacion;
+
+t_log* loggerDev;
+t_config* config;
+t_log* loggerGameCard;
+
+char* rutaMetaData;
+char* rutaFiles;
+char* rutaBlocks;
+
+struct stat st1;
+
+
+typedef enum
+{
+	NEW          = 1,
+    APPEARED     = 2,
+    CATCH        = 3,
+    CAUGHT       = 4,
+	GET          = 5,
+	LOCALIZED	 = 6,
+	SUSCRIPTOR 	 = 7,
+	MENSAJE 	 = 8//TEST
+}op_code;
+
+typedef enum
+{
+	BROKER       = 0,
+	TEAM	     = 1,
+	GAMECARD     = 2,
+	SERVER		 = 3 //TEST
+}op_codeModulo;
+
+
+typedef enum
+{
+	METADATA =1,
+	BITMAP = 2,
+	METADATA_FILE = 3,
+	METADATA_POKEMON = 4,
+	BLOCK = 5
+}tipoArchivo;
+
+
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	op_code codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+
+
+//########################################################### MENSAJE NEW - APPEARED ###########################################################
+
+// New ‘Pikachu’ 5 10 2
+typedef struct
+{
+	uint32_t idMensaje;
+    uint32_t sizePokemon;
+	char*	 pokemon;
+    uint32_t  coordX;
+    uint32_t  coordY;
+    uint32_t cantidad;
+}t_new;
+
+
+// Appeared ‘Pikachu’ 1 5
+typedef struct
+{
+	uint32_t sizePokemon;
+	char*	 pokemon;
+	uint32_t  coordX;
+	uint32_t  coordY;
+}t_appeared;
+
+//###############################################################################################################################################
+
+
+
+
+//########################################################### MENSAJE GET - LOCALIZED ###########################################################
+
+typedef struct
+{
+	uint32_t sizePokemon;
+    uint32_t  coordX;
+    uint32_t  coordY;
+}t_get;
+
+
+// Localized ‘Pikachu’ 3 4 5 1 5 9 3
+typedef struct
+{
+	uint32_t sizePokemon;
+	char*	 pokemon;
+    uint32_t lugares;
+    uint32_t  coordX;
+    uint32_t  coordY;
+
+}t_localized;
+
+//##############################################################################################################################################
+
+
+
+
+//########################################################### MENSAJE CATCH - CAUGHT ###########################################################
+
+// Catch ‘Pikachu’ 1 5
+typedef struct
+{
+	uint32_t sizePokemon;
+	char*	 pokemon;
+    uint32_t  coordX;
+    uint32_t  coordY;
+}t_catch;
+
+typedef struct{
+
+ uint32_t flagCaught;
+
+ }t_caught;
+
+//##################################################################################################################################################
+
+
+
+int crear_conexion(char* ip, char* puerto);
+void enviar_mensaje(char*argv[], int socket_cliente);
+void recibir_mensaje(int socket_cliente);
+void crearDirectorio(char*, char*);
+void eliminar_paquete(t_paquete* paquete);
+void liberar_conexion(int socket_cliente);
 
 
 #endif /* UTILSGAMECARD_H_ */
