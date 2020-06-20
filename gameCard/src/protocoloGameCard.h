cb786c8ef5c558319e7bb408918d2dc5c8d67eff
FacuTadic <facutadich@gmail.com>
2020-06-20 20:23:19 -0300

    Elimino datos innecesarios del protocolo de recv

--- gameCard/src/protocoloGameCard.h
+++ gameCard/src/protocoloGameCard.h
@@ -12,37 +12,19 @@
 #include<pthread.h>
 
 
-
-
-///////////
-// MENSAJES
-///////////
-
 typedef struct {
     char* pokemon;
     uint32_t pos_X, pos_Y, cantidad;
     uint32_t id;
 } t_newLlegada;
 
-typedef struct {
-	uint32_t id_correlativo;
-    char* pokemon;
-    uint32_t lugares;
-    t_list* l_coordenadas;
-    uint32_t id;
-} t_localizedLlegada;
+
 
 typedef struct {
     char* pokemon;
     uint32_t id;
 } t_getLlegada;
 
-typedef struct {
-	uint32_t id_correlativo;
-	char* pokemon;
-    uint32_t pos_X, pos_Y;
-    uint32_t id;
-} t_appearedLlegada;
 
 typedef struct {
     char* pokemon;
@@ -50,17 +32,6 @@ typedef struct {
     uint32_t id;
 } t_catchLlegada;
 
-typedef struct {
-	uint32_t id_correlativo;
-	uint32_t flag_caught;
-	uint32_t id;
-} t_caughtLlegada;
-
-
-
-//////////
-// MODULOS
-//////////
 
 typedef struct {
 	uint32_t id_cliente;
@@ -68,11 +39,8 @@ typedef struct {
 } info_modulo;
 
 void recibir_new(int socket_cliente, uint32_t* size, t_log* logger);
-void recibir_appeared(int socket_cliente, uint32_t* size, t_log* logger);
 void recibir_catch(int socket_cliente, uint32_t* size, t_log* logger);
-void recibir_caught(int socket_cliente, uint32_t* size, t_log* logger);
 void recibir_get(int socket_cliente, uint32_t* size, t_log* logger);
-void recibir_localized(int socket_cliente, uint32_t* size, t_log* logger);
 void devolver_id(int socket_cliente, uint32_t id, t_log* logger);
 
 
