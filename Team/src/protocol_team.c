ca622e0a8cd94f084c73d4d0663b719b45d1cf6e
bochita <mateobozz@gmail.com>
2020-07-13 23:58:50 -0300

    modifico gilada

--- Team/src/protocol_team.c
+++ Team/src/protocol_team.c
@@ -173,7 +173,7 @@ t_caught* recibir_caught(int socket_broker, uint32_t* size, t_log* logger) {
 
 	status_recv = recv(socket_broker, caught->idCorrelativo, sizeof(uint32_t), MSG_WAITALL);
 
-	if (!list_find(catch_IDs ,caught->idCorrelativo)){    							 // si el id del caught no esta en la lista de catch recibidos, ignora el mensaje
+	if (list_find(catch_IDs ,caught->idCorrelativo) == NULL){    							 // si el id del caught no esta en la lista de catch recibidos, ignora el mensaje
 		log_info(logger, "el ID de caught no corresponde a ningun catch, mensaje ignorado");
 		free(caught);
 		return NULL;
