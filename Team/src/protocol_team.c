28a4c2002c53922d55d14b89923203bd0f3cdc5b
NicolasMatiasFasce <nicofasce1996@gmail.com>
2020-07-14 00:15:40 -0300

    gilada

--- Team/src/protocol_team.c
+++ Team/src/protocol_team.c
@@ -169,15 +169,7 @@ t_caught* recibir_caught(int socket_broker, uint32_t* size, t_log* logger) {
 
 	log_info(logger, "Tamanio total recibido: %i", *size);
 
-
-
 	status_recv = recv(socket_broker, caught->idCorrelativo, sizeof(uint32_t), MSG_WAITALL);
-
-	if (!list_find(catch_IDs ,caught->idCorrelativo)){    							 // si el id del caught no esta en la lista de catch recibidos, ignora el mensaje
-		log_info(logger, "el ID de caught no corresponde a ningun catch, mensaje ignorado");
-		free(caught);
-		return NULL;
-	}
 	if (status_recv == -1) {
 		close(socket_broker);
 		log_error(logger, "Hubo un problema recibiendo el id correlativo");
@@ -207,14 +199,28 @@ t_caught* recibir_caught(int socket_broker, uint32_t* size, t_log* logger) {
 		return NULL;
 	}
 
+	int es_el_id_buscado(void* id_lista) {
+		if (((uint32_t) id_lista) == caught->idCorrelativo) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	// si el id del caught no esta en la lista de catch recibidos, ignora el mensaje
+	if (list_find(catch_IDs, (void*) es_el_id_buscado) == NULL) {
+		log_info(logger, "el ID correlativo %i de caught no corresponde a ningun catch, mensaje ignorado", caught->idCorrelativo);
+		free(caught);
+		devolver_ack(socket_broker,logger);
+		return NULL;
+	}
+
 	log_info(logger, "Flag atrapado recibido: %i", caught->flag);
 
 	devolver_ack(socket_broker,logger);
 
 	return caught;
 }
-
-
 
 t_localized* recibir_localized(int socket_broker, uint32_t* size, t_log* logger) {
 	t_localized* localized = malloc(sizeof(t_localized));
