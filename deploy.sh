592180cc177ad18c159781f5e93001ae0941eb81
NicolasMatiasFasce <nicofasce1996@gmail.com>
2020-07-25 15:02:14 -0300

    Modifico el script de deploy

--- deploy.sh
+++ deploy.sh
@@ -16,3 +16,11 @@ sudo ./buildBroker.sh
 sudo ./buildGameCard.sh
 sudo ./buildGameBoy.sh
 sudo ./buildTeam.sh
+
+cd
+cd Proyectos/tp-2020-1c-Postre-de-vainilla-DIA-
+mkdir tall-grass
+cd tall-grass
+mkdir Metadata
+cd Metadata
+touch Metadata.bin
